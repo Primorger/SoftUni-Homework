a3795edb3b8ed96d910e168a518a069b9490f309
Primorger <sharma.suan@gmail.com>
2026-02-27 16:21:01 +0200

    Programmer laid out

---
+++ Soft_Uni/Python_OOP_2026/Exercise_First_Steps_In_OOP/Programmer.py
@@ -0,0 +1,9 @@
+class Programmer:
+    def __init__(self, name, language, skills):
+        self.name = name
+        self.language = language
+        self.skills = skills
+    def watch_course(self, course_name, language, skills_earned):
+        pass
+    def change_language(self, new_language, skills_needed):
+        pass
