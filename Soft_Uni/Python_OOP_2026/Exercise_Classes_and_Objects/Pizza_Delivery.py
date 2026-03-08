33c5a4314b24f390af2e3c71f1307e084a08ae61
Primorger <sharma.suan@gmail.com>
2026-03-08 20:24:55 +0200

    pizza delivery

---
+++ Soft_Uni/Python_OOP_2026/Exercise_Classes_and_Objects/Pizza_Delivery.py
@@ -0,0 +1,39 @@
+class PizzaDelivery:
+    def __init__(self, name: str, price: float, ingredients: dict):
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients
+        self.ordered = False
+
+    def add_extra(self, ingredient: str, quantity: int, price_per_quantity: float) -> str:
+        if self.ordered:
+            return f"Pizza {self.name} already prepared, and we can't make any changes!"
+        
+        if ingredient in self.ingredients:
+            self.ingredients[ingredient] += quantity
+        else:
+            self.ingredients[ingredient] = quantity
+        
+        self.price += quantity * price_per_quantity
+
+    def remove_ingredient(self, ingredient: str, quantity: int, price_per_quantity: float) -> str:
+        if self.ordered:
+            return f"Pizza {self.name} already prepared, and we can't make any changes!"
+        
+        if ingredient not in self.ingredients:
+            return f"Wrong ingredient selected! We do not use {ingredient} in {self.name}!"
+        
+        if self.ingredients[ingredient] < quantity:
+            return f"Please check again the desired quantity of {ingredient}!"
+        
+        self.ingredients[ingredient] -= quantity
+        self.price -= quantity * price_per_quantity
+
+    def make_order(self) -> str:
+        if self.ordered:
+            return f"Pizza {self.name} already prepared, and we can't make any changes!"
+        
+        self.ordered = True
+        
+        ingredients_str = ", ".join(f"{ing}: {qty}" for ing, qty in self.ingredients.items())
+        return f"You've ordered pizza {self.name} prepared with {ingredients_str} and the price will be {self.price}lv."
