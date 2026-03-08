640514e5757cae54af4e9a7c51a68acb1ec717ea
Primorger <sharma.suan@gmail.com>
2026-03-08 19:58:48 +0200

    guild sys

---
+++ Soft_Uni/Python_OOP_2026/Exercise_Classes_and_Objects/Guild_System/project/player.py
@@ -0,0 +1,19 @@
+class Player:
+    def __init__(self, name: str, hp: int, mp: int):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.skills: dict = {}
+        self.guild = "Unaffiliated"
+
+    def add_skill(self, skill_name: str, mana_cost: int) -> str:
+        if skill_name in self.skills:
+            return "Skill already added"
+        self.skills[skill_name] = mana_cost
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self) -> str:
+        result = f"Name: {self.name} Guild: {self.guild} HP: {self.hp}\nMP: {self.mp}"
+        for skill_name, mana_cost in self.skills.items():
+            result += f"\n==={skill_name} - {mana_cost}"
+        return result
