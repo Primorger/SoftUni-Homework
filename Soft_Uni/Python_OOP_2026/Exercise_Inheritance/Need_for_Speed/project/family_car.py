8eae6269901857bcc2924f52b108bbe31c998fe8
Primorger <sharma.suan@gmail.com>
2026-03-11 22:04:37 +0200

    Need for speed done

---
+++ Soft_Uni/Python_OOP_2026/Exercise_Inheritance/Need_for_Speed/project/family_car.py
@@ -0,0 +1,5 @@
+from project.car import Car
+
+
+class FamilyCar(Car):
+    DEFAULT_FUEL_CONSUMPTION = 3
---
+++ Soft_Uni/Python_OOP_2026/Exercise_Inheritance/Need_for_Speed/project/sport_car.py
@@ -0,0 +1,5 @@
+from project.vehicle import Vehicle
+
+
+class SportCar(Vehicle):
+    DEFAULT_FUEL_CONSUMPTION = 10
