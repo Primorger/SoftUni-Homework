4577077ff37f73e4dd03ad5034d5f0f33345260f
Primorger <sharma.suan@gmail.com>
2026-03-29 01:10:08 +0200

    take skip done

---
+++ Soft_Uni/Python_OOP_2026/Exercise_Iterators_and_Generators/Take_Skip.py
@@ -0,0 +1,20 @@
+class take_skip:
+    def __init__(self, step: int, count: int):
+        self.step = step
+        self.count = count
+        self.__counted = -1
+
+    def __iter__(self):
+        return self
+        
+    def __next__(self):
+        self.__counted += 1
+        if self.__counted == self.count:
+            raise StopIteration()
+        return self.__counted * self.step
+
+#------------- Test code -------------#
+
+numbers = take_skip(100, 10)
+for number in numbers:
+    print(number)
